e771837e128fc61712218d0b768eab1a66b3b9da
Nikita Epifantsev <eppy_92@mail.ru>
2023-03-13 15:10:59 +0300

    third commit

---
+++ FirstUniqueChar/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        foreach (var itm in s.Distinct())
+        {
+            if (s.Count(x => x == itm) == 1)
+            {
+                return s.IndexOf(itm);
+            }
+        }
+        return -1;
+
+    }
+}
